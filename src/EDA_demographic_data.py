f6f43d6a503237a9a4074fc913ef9fecca77e453
Jessica Dafflon <jejedafflon@gmail.com>
2019-04-21 10:26:32 +0100

    Demographic analysis
    * Also plot the general demographic of all subjects, independet of the site

--- src/EDA_demographic_data.py
+++ src/EDA_demographic_data.py
@@ -4,16 +4,28 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import os
+sns.set(style="whitegrid")
 
-PROJECT_ROOT = Path('/home/walter/Desktop/mind-the-gap')
-
-demographic_filename = PROJECT_ROOT / 'data' / 'PAC2019_BrainAge_Training.csv'
-output_dir = PROJECT_ROOT / 'output' / 'EDA'
+cwd = os.getcwd()
+demographic_filename = os.path.join(cwd,'data','PAC2019_BrainAge_Training.csv')
+output_dir = os.path.join(cwd, 'output', 'EDA')
 
 demographic_df = pd.read_csv(demographic_filename)
+
+# Plot age distribution pro site
 a4_dims = (11.7, 8.27)
 fig, ax = plt.subplots(figsize=a4_dims)
-sns.set(style="whitegrid")
 sns.violinplot(y='site', x='age', data=demographic_df, hue='gender', split=True, scale='count', orient='h', ax=ax)
-plt.savefig(output_dir / 'age_ranges.png')
+plt.savefig(os.path.join(output_dir, 'age_ranges_site.png'))
+
+# Plot age distribution of the entire dataset
+plt.figure()
+min_age = demographic_df['age'].min()
+max_age = demographic_df['age'].max()
+plt.hist(demographic_df['age'], bins=int(max_age-min_age), range=(min_age, max_age))
+plt.xlabel('Age')
+plt.ylabel('# of Subjects')
+plt.savefig(os.path.join(output_dir, 'age_ranges_distribution.png'))
+
 # plt.show()
