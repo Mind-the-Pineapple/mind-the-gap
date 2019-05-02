385d8f65a44515ae6ac663aec10bb4d2efa30ecd
Jessica Dafflon <jejedafflon@gmail.com>
2019-05-02 15:39:59 +0100

    Singularity: Create run and exec files

--- singularity/exec_sing.sh
+++ singularity/exec_sing.sh
@@ -2,6 +2,6 @@
 
 datapath=/data/project/mindgap/data
 singularity exec -c \
-                -B ~/Code/mind-the-gap:\code \
-                -B $datapath:\code/data \
-                $(dirname$0)/mind.img "$@"
+                -B ~/Code/mind-the-gap:/code \
+                -B $datapath:/code/data \
+                $(dirname $0)/mind.img "$@"
